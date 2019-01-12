7e5723c5b61d30b012cc5a5d62e3cde83ac45fb3
Timothy Yuan <timothyyuan@Timothy-Yuans-MacBook-Pro.local>
2019-01-12 01:21:40 -0800

    change sql

--- data.sql
+++ data.sql
@@ -55,7 +55,7 @@ CREATE TABLE `users` (
   `password` varchar(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_name` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
