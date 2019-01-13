4c5f5ce346a631cf3869152e7477653e922d6444
Timothy Yuan <timothyyuan@Timothy-Yuans-MacBook-Pro.local>
2019-01-13 04:06:47 -0800

    final

--- server/test.py
+++ server/test.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-url = "http://35.235.88.148:3000/check"
-file = open('upload.dat', 'rb').read()
-r = requests.post(url, file = file)
+url = "http://localhost:3000/check"
+files = {'file': open('upload.dat', 'rb')}
+r = requests.post(url, files=files)
 data = r.json()['isAllowed']
 print(data)
